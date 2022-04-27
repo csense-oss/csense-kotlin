2ef314d5849b269ced45e697025454379a1388f8
Kasper T <kasper@webmasteren.eu>
2022-04-27 18:26:08 +0200

    added experimental debugging helpers for jvm

---
+++ src/jvmMain/kotlin/csense/kotlin/debugging/JvmDebugging.kt
@@ -0,0 +1,82 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package csense.kotlin.debugging
+
+import csense.kotlin.annotations.*
+
+@ExperimentalCsenseApi
+@JvmInline
+public value class Debugging(
+    public val caller: StackTraceElement
+) {
+    public companion object {
+
+        @Throws(RuntimeException::class)
+        public fun fromCallingMethod(): Debugging {
+            val stackTraceElement: StackTraceElement = Thread.currentThread()
+                .stackTrace
+                .findCallerMethod() ?: throw RuntimeException("Could not find caller method")
+            return Debugging(stackTraceElement)
+        }
+
+        private fun Array<StackTraceElement>.findCallerMethod(): StackTraceElement? {
+            val ourClassName = this@Companion::class.java.name
+            return findItemFollowingSeriesBy {
+                it.className.equals(ourClassName)
+            }
+        }
+
+        /**
+         * Searches (incrementally) for a "series" (predicate returns true) and then returns the element following that series (if any).
+         * @receiver Array<T>
+         * @param predicate Function1<T, Boolean>
+         * @return T?
+         */
+        //TODO consider this as a "searching" specific extension etc?
+        private fun <T> Array<T>.findItemFollowingSeriesBy(predicate: (T) -> Boolean): T? {
+            var haveFoundSeries = false
+            forEach { item ->
+                val isSeries = predicate(item)
+                if (haveFoundSeries && !isSeries) {
+                    return@findItemFollowingSeriesBy item
+                }
+                haveFoundSeries = isSeries
+            }
+            return null
+        }
+    }
+}
+
+@ExperimentalCsenseApi
+public inline val debugging: Debugging
+    get() = Debugging.fromCallingMethod()
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodName(): String {
+    return caller.methodName
+}
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodLineNumber(): Int {
+    return caller.lineNumber
+}
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodFileName(): String {
+    return caller.fileName ?: ""
+}
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodFileNameAndLineNumber(): String {
+    return "${caller.fileName ?: "-"}:${caller.lineNumber}"
+}
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodCanonicalName(): String {
+    return caller.className + "." + caller.methodName
+}
+
+@OptIn(ExperimentalCsenseApi::class)
+public inline fun Debugging.getCurrentMethodInformation(): String {
+    return caller.toString()
+}
---
+++ src/jvmTest/kotlin/csense/kotlin/debugging/JvmDebuggingTest.kt
@@ -0,0 +1,59 @@
+package csense.kotlin.debugging
+
+import csense.kotlin.tests.assertions.*
+import org.junit.*
+
+class JvmDebuggingTest {
+    @Test
+    fun getCurrentMethodName() {
+        val name = this::getCurrentMethodName.name
+        debugging.getCurrentMethodName().assert(name)
+    }
+
+    @Test
+    fun getCurrentMethodLineNumber() {
+        //This is EXTREMLY flaky, so just "verify" it roughly
+        debugging.getCurrentMethodLineNumber().assertLargerThan(0)
+    }
+
+    @Test
+    fun getCurrentMethodFileName() {
+        //This is somewhat flaky, so just "verify" it roughly
+        debugging.getCurrentMethodFileName().assert(guessCurrentFileName())
+    }
+
+    @Test
+    fun getCurrentMethodFileNameAndLineNumber() {
+        //This is EXTREMLY flaky, so just "verify" it roughly
+        val filename = guessCurrentFileName()
+        val filenameAndLineNumber = debugging.getCurrentMethodFileNameAndLineNumber()
+        filenameAndLineNumber.assertStartsWith("${filename}:")
+    }
+
+    @Test
+    fun getCurrentMethodCanonicalName() {
+        val currentClassName = getCurrentClassNameCanonical()
+        val currentMethodName = this::getCurrentMethodCanonicalName.name
+        debugging.getCurrentMethodCanonicalName().assert("$currentClassName.$currentMethodName")
+    }
+
+    @Test
+    fun getCurrentMethodInformation() {
+        val currentClassName = getCurrentClassNameCanonical()
+        val currentMethodName = this::getCurrentMethodInformation.name
+        val currentFileName = guessCurrentFileName()
+        val info = debugging.getCurrentMethodInformation()
+
+        //info should have the format: $classname.$methodName($filename:$lineNumber)
+        info.assertStartsWith("$currentClassName.$currentMethodName($currentFileName:")
+        info.assertEndsWith(")")
+    }
+
+    private fun getCurrentClassNameSimple(): String = this::class.simpleName ?: ""
+    private fun getCurrentClassNameCanonical(): String = this::class.qualifiedName ?: ""
+    private fun guessCurrentFileName(): String = guessFileNameFromClassName(getCurrentClassNameSimple())
+
+    private fun guessFileNameFromClassName(className: String): String {
+        return "$className.kt"
+    }
+}
