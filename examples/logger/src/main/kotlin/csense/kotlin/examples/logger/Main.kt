d2a2a047128db6160bdb1bc7dd9fbd28a021b03f
Kasper T <kasper@webmasteren.eu>
2022-09-02 01:27:07 +0200

    make it build

--- examples/logger/src/main/kotlin/csense/kotlin/examples/logger/Main.kt
+++ examples/logger/src/main/kotlin/csense/kotlin/examples/logger/Main.kt
@@ -1,73 +1,72 @@
 package csense.kotlin.examples.logger
 
-import csense.kotlin.logger.*
 import kotlinx.coroutines.*
 
 fun main(): Unit = runBlocking {
-
-    if (true) {
-//        CL.enableSensitiveLogging()
-    }
-
-//    CL.printLogsToConsole()
-    CL.printLogsToConsoleAnsiColored()
-
-
-//    async {
-//        CL.allLoggers.filter { it.doesNotContainSensitiveInformation() }.collect {
-//            delay(1000)
 //
-//        }
+//    if (true) {
+////        CL.enableSensitiveLogging()
 //    }
-//    async {
-//        CL.allLoggers.collect {
-//            val sensitive = it.throwable?.toSensitiveStackTraceString()
-//            println("got sensitive")
-//            println("$sensitive")
-//        }
-//    }
-
-//    async {
-//        val sharedIn = CLogger.debugLoggers.shareIn(
-//            this,
-//            SharingStarted.Eagerly,
-//            3
-//        )
-//        sharedIn.collect {
-//            println("sharedInCache = ${sharedIn.replayCache.size}")
-//        }
+//
+////    CL.printLogsToConsole()
+//    CL.printLogsToConsoleAnsiColored()
 //
 //
-//    }
+////    async {
+////        CL.allLoggers.filter { it.doesNotContainSensitiveInformation() }.collect {
+////            delay(1000)
+////
+////        }
+////    }
+////    async {
+////        CL.allLoggers.collect {
+////            val sensitive = it.throwable?.toSensitiveStackTraceString()
+////            println("got sensitive")
+////            println("$sensitive")
+////        }
+////    }
+//
+////    async {
+////        val sharedIn = CLogger.debugLoggers.shareIn(
+////            this,
+////            SharingStarted.Eagerly,
+////            3
+////        )
+////        sharedIn.collect {
+////            println("sharedInCache = ${sharedIn.replayCache.size}")
+////        }
+////
+////
+////    }
+////    async {
+////        while (true) {
+////            CL.logDebug(
+////                "MyTag",
+////                "MyMessage using Person identifier {}",
+////                "000000-0000",
+////                //   throwable = RuntimeException()
+////            )
+////            delay(3000)
+////        }
+////    }
+//
+////    logClassDebug("Message {}", "someMessage")
 //    async {
 //        while (true) {
-//            CL.logDebug(
-//                "MyTag",
-//                "MyMessage using Person identifier {}",
-//                "000000-0000",
-//                //   throwable = RuntimeException()
+//            logClassDebug(
+//                "Message {} {}",
+//                "someMessage", "mis",
+//                throwable = RuntimeException("sensitive information potentially"),
+//                sensitivity = LogSensitivity.Sensitive
 //            )
+////            CLogger.logDebug(
+////                "FavoriteFood",
+////                "NonPersonal info: meal type={}",
+////                "meat",
+////                sensitivity = LogSensitivity.Insensitive
+////                //   throwable = RuntimeException()
+////            )
 //            delay(3000)
 //        }
 //    }
-
-//    logClassDebug("Message {}", "someMessage")
-    async {
-        while (true) {
-            logClassDebug(
-                "Message {} {}",
-                "someMessage", "mis",
-                throwable = RuntimeException("sensitive information potentially"),
-                sensitivity = LogSensitivity.Sensitive
-            )
-//            CLogger.logDebug(
-//                "FavoriteFood",
-//                "NonPersonal info: meal type={}",
-//                "meat",
-//                sensitivity = LogSensitivity.Insensitive
-//                //   throwable = RuntimeException()
-//            )
-            delay(3000)
-        }
-    }
 }
