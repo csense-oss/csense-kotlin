8e357911001c6498ab09279583da8037faf4e390
Kasper T <kasper@webmasteren.eu>
2022-10-18 22:04:40 +0200

    somewhat make the tests "work"

--- src/commonTest/kotlin/csense/kotlin/patterns/restartableJob/RestartableJobTest.kt
+++ src/commonTest/kotlin/csense/kotlin/patterns/restartableJob/RestartableJobTest.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
 package csense.kotlin.patterns.restartableJob
 
 import csense.kotlin.tests.assertions.*
@@ -19,34 +21,38 @@ class RestartableJobTest {
     }
 
     @Test
-    fun isRunning() = runTest {
-        assertCalled { shouldBeCalled ->
-            val job = RestartableJob(this, Dispatchers.Default) {
-                shouldBeCalled()
-                delay(5000)
-            }
-            job.isRunning().assertFalse("should be lazy by default")
-            job.start()
-            job.isRunning().assertTrue("should start when asked to")
-            advanceTimeBy(1000)
-            job.isRunning().assertTrue("should be running while not done")
-            advanceUntilIdle()
-            job.isRunning().assertFalse("should stop when done")
+    fun isRunning(): TestResult {
+        val testDispatcher = StandardTestDispatcher()
 
-            job.start()
-            job.isRunning().assertTrue("should start when asked to")
-            job.cancel()
-            job.isRunning().assertFalse("should stop when cancel is called")
+        return runTest(testDispatcher) {
+            assertCalled(times = 1) { shouldBeCalled ->
+                val job = RestartableJob(this, testDispatcher) {
+                    shouldBeCalled()
+                    delay(5000)
+                }
+                job.isRunning().assertFalse("should be lazy by default")
+                job.start()
+                job.isRunning().assertTrue("should start when asked to")
+                advanceTimeBy(1000)
+                job.isRunning().assertTrue("should be running while not done")
+                advanceUntilIdle()
+                job.isRunning().assertFalse("should stop when done")
+
+                job.start()
+                job.isRunning().assertTrue("should start when asked to")
+                job.cancel()
+                job.isRunning().assertFalse("should stop when cancel is called")
+            }
         }
     }
 
     @Test
     fun cancel() = runTest {
-        assertCalled { shouldBeCalled ->
-            val job = RestartableJob(this, Dispatchers.Default) {
-                shouldBeCalled()
-            }
+        val job = RestartableJob(this, Dispatchers.Default) {
+            shouldNotBeCalled()
         }
+        job.cancel()
+        job.join()
     }
 
     @Test
@@ -55,6 +61,8 @@ class RestartableJobTest {
             val job = RestartableJob(this, Dispatchers.Default) {
                 shouldBeCalled()
             }
+            job.start()
+            job.join()
         }
     }
 }
