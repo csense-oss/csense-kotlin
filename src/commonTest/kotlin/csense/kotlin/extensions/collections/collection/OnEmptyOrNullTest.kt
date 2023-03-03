5b43963892e9c945618624c6589acde8f112e281
Kasper T <kasper@webmasteren.eu>
2023-03-03 19:11:01 +0100

    Create OnEmptyOrNullTest.kt

---
+++ src/commonTest/kotlin/csense/kotlin/extensions/collections/collection/OnEmptyOrNullTest.kt
@@ -0,0 +1,64 @@
+package csense.kotlin.extensions.collections.collection
+
+import csense.kotlin.tests.assertions.*
+import kotlin.test.*
+
+class OnEmptyOrNullTest {
+    class CollectionItemOnNullOrEmptyItem {
+        @Test
+        fun nullable() {
+            val lst: List<String>? = null
+            lst.onNullOrEmpty(item = "test").assertSingle("test")
+        }
+
+        @Test
+        fun empty() {
+            listOf<String>().nullable().onNullOrEmpty(item = "test").assertSingle("test")
+        }
+
+        @Test
+        fun single() {
+            listOf("starting").nullable().onNullOrEmpty(item = "failed").assertSingle("starting")
+        }
+
+        @Test
+        fun multiple() {
+            listOf("a", "b", "c").nullable().onNullOrEmpty(item = "failed").assertContentAndOrder(
+                expected = listOf(
+                    "a",
+                    "b",
+                    "c"
+                )
+            )
+        }
+    }
+
+    class CollectionItemOnNullOrEmptyItems {
+        @Test
+        fun nullable() {
+            val lst: List<String>? = null
+            lst.onNullOrEmpty(items = listOf("items")).assertSingle("items")
+        }
+
+        @Test
+        fun empty() {
+            listOf<String>().nullable().onNullOrEmpty(items = listOf("test")).assertSingle("test")
+        }
+
+        @Test
+        fun single() {
+            listOf("starting").nullable().onNullOrEmpty(items = listOf("failed")).assertSingle("starting")
+        }
+
+        @Test
+        fun multiple() {
+            listOf("a", "b", "c").nullable().onNullOrEmpty(items = listOf("failed")).assertContentAndOrder(
+                expected = listOf(
+                    "a",
+                    "b",
+                    "c"
+                )
+            )
+        }
+    }
+}
