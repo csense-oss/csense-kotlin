51eb2e78812423a1f99856594601dbcc111adf9f
Kasper T <kasper@webmasteren.eu>
2023-03-03 16:45:02 +0100

    Update OnEmptyOrNull.kt

--- src/commonMain/kotlin/csense/kotlin/extensions/collections/collection/OnEmptyOrNull.kt
+++ src/commonMain/kotlin/csense/kotlin/extensions/collections/collection/OnEmptyOrNull.kt
@@ -1,7 +1,7 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
+@file:Suppress("UNUSED_PARAMETER", "NOTHING_TO_INLINE", "UnusedReceiverParameter","INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
 package csense.kotlin.extensions.collections.collection
 
+import csense.kotlin.general.*
 import kotlin.jvm.*
 
 
@@ -34,3 +34,33 @@ public inline fun <Item> Collection<Item>?.onNullOrEmptyLazy(action: () -> Colle
     }
     return action()
 }
+
+@Deprecated(
+    "Receiver known at compile time to not be null. Use onEmpty instead",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("onEmpty(item)", "csense.kotlin.extensions.collections.collection")
+)
+public inline fun Collection<Any>.onNullOrEmpty(item: Any): Nothing = unexpected()
+
+@Deprecated(
+    "Receiver known at compile time to not be null. Use onEmpty instead",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("onEmpty(items)", "csense.kotlin.extensions.collections.collection")
+)
+public inline fun Collection<Any>.onNullOrEmpty(items: Collection<Any>): Nothing = unexpected()
+
+@Deprecated(
+    "Receiver known at compile time to not be null. Use onEmpty instead",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("onEmptyLazy(item)", "csense.kotlin.extensions.collections.collection")
+)
+public inline fun Collection<Any>.onNullOrEmptyLazy(item: Any): Nothing = unexpected()
+
+
+@Deprecated(
+    "Receiver known at compile time to not be null. Use onEmpty instead",
+    level = DeprecationLevel.ERROR,
+    replaceWith = ReplaceWith("onEmptyLazy(items)", "csense.kotlin.extensions.collections.collection")
+)
+public inline fun Collection<Any>.onNullOrEmptyLazy(items: Collection<Any>): Nothing = unexpected()
+
